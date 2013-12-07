0cde83bd177f2ac2f49efe5c6c671864900930a1
Stephen Quenzer <swquenzer@gmail.com>
2013-12-07 17:47:50 -0500

    Move addInventory processing to addInventory.php
    -And other smaller updates

--- addInventory.php
+++ addInventory.php
@@ -55,43 +55,78 @@ if( !isset( $_SESSION["loggedIn"] ) )
         </header>
         <div id="wrapper">
                 <section id="main">
-                        <form class="inputField" id="addInv" action="Function/tbl.php" method="post"><fieldset>
-                                <p>
-                                 <label id="manufacturerLbl">Manufacturer</label>
-                                 <input list="manufacturers" name="manufacturer" />
-                                </p><p>
-                                 <label id="chemicalsLbl">Chemical</label>
-                                 <input list="chemicals" name="chemical" />
-                                </p><p>
-                                 <label id="roomLbl">Room</label>
-                                 <input list="rooms" name="room" />
-                                </p><p>
-                                 <label id="locationLbl">Location</label>
-                                 <input list="location" name="location" />
-                                </p><p>
-                                 <label id="quantLbl">Quantity</label>
-                                 <input list="quantity" name="quant" />
-                                </p><p>
-                    <label id="unitSizeLbl">Unit Size</label>
-                                 <input type="number" name="unitSize" />
-                </p><p>
-                    <label id="unitLbl">Unit of Measure</label>
-                    <input type="text" name="unit">
-                                </p><p>
-                                <input type="submit" name="submit">
-                                </p>
-                                <!--these will be filled by javascript when the page loads-->
-                                <datalist id="manufacturers">
-                                </datalist>
-                                <datalist id="chemicals">
-                                </datalist>
-                                <datalist id="rooms">
-                                </datalist>
-                                <datalist id="quantity">
-                                </datalist>
-                                <datalist id="location">
-                                </datalist>
-                        </fieldset></form>
+				<?php
+					if(isset($_POST['submit'])) {
+						require('function/logger.php');
+						require('admin/AcidRainDBLogin.php');
+						#Get chemical id ( need to create better query with join later )
+						$query = $db->prepare("SELECT ID FROM chemical WHERE Name=?");
+						$query->bind_param('s', $_POST['chemical']);
+						$query->execute(); //necessary?
+						$result = $query->get_result(); //Error Checking Needed
+						$result = $result->fetch_array(MYSQLI_BOTH);
+						$chemID = $result[0];
+						$query->close();
+						#Validate
+						require 'function/validate.php';
+						$errors = validateAddInventory($_POST['room'], $_POST['quant'], $chemID, $_POST['unitSize'], $_POST['unit']);
+						if(empty($errors)) {
+							#Insert record
+							$query = $db->prepare("INSERT INTO inventory (Room, Location, ItemCount, ChemicalID, Size, Units, LastUpdated) VALUES (?, ?, ?, ?, ?, ?, ?)");
+							$currentDate = date("Y-m-d H:i:s");
+							$_POST['quant'] = (int) $_POST['quant'];
+							$_POST['unitSize'] = (int) $_POST['unitSize'];
+							$query->bind_param('ssiiiss', $_POST['room'], $_POST['location'], $_POST['quant'], $chemID, $_POST['unitSize'], $_POST['unit'], $currentDate);
+							if(!$query->execute()) {
+									slog('problem executing query in tbl.php: ' . $db->error);
+							}
+						}
+					}
+				?>
+					<form class="inputField" id="addInv" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post"><fieldset>
+							<?php
+								echo "<span class='errMsg'><h3 style='margin-left: 20px;'>Errors:</h3><ul>";
+								foreach($errors as $e) {
+										echo "<li>$e</li>";
+								}
+								echo "</ul></span>";
+							?>
+							<p>
+							 <label id="manufacturerLbl">Manufacturer</label>
+							 <input list="manufacturers" name="manufacturer" required />
+							</p><p>
+							 <label id="chemicalsLbl">Chemical</label>
+							 <input list="chemicals" name="chemical" required />
+							</p><p>
+							 <label id="roomLbl">Room</label>
+							 <input list="rooms" name="room" required />
+							</p><p>
+							 <label id="locationLbl">Location</label>
+							 <input list="location" name="location" required />
+							</p><p>
+							 <label id="quantLbl">Quantity</label>
+							 <input list="quantity" name="quant" required />
+							</p><p>
+							 <label id="unitSizeLbl">Unit Size</label>
+							 <input type="number" name="unitSize" required />
+							</p><p>
+							 <label id="unitLbl">Unit of Measure</label>
+							 <input type="text" name="unit"required >
+							</p><p>
+							 <input type="submit" name="submit">
+							</p>
+							<!--these will be filled by javascript when the page loads-->
+							<datalist id="manufacturers">
+							</datalist>
+							<datalist id="chemicals">
+							</datalist>
+							<datalist id="rooms">
+							</datalist>
+							<datalist id="quantity">
+							</datalist>
+							<datalist id="location">
+							</datalist>
+					</fieldset></form>
                 </section>
         </div><!--End Wrapper-->
         <footer>
